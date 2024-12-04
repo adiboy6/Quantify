a3ba1089fbaeec6c612529acb9016ddd8cfd29a0
adiboy6 <aditya.reddy.97@gmail.com>
2024-12-04 13:07:29 -0500

    removed server proxy

--- dashboard/frontend/vite.config.js
+++ dashboard/frontend/vite.config.js
@@ -4,14 +4,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
