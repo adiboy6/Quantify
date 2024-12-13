c140c21fee60e60f258216ef98c160181d4c497f
adiboy6 <aditya.reddy.97@gmail.com>
2024-12-13 07:50:50 -0500

    minor updates for cover letter

--- dashboard/backend/app.py
+++ dashboard/backend/app.py
@@ -104,33 +104,25 @@ def get_profile():
         return jsonify({"error": str(e)}), 500 
     
 def create_formatted_pdf(cover_letter):
-    # Replace newline characters with <br /> for line breaks in PDF
     formatted_text = cover_letter.replace("\n", "<br />")
 
-    # Create an in-memory buffer
     buffer = io.BytesIO()
 
-    # Create the PDF document
     doc = SimpleDocTemplate(buffer, pagesize=letter)
 
-    # Define a style for the text
     styles = getSampleStyleSheet()
     style = styles["Normal"]
     
-    # Create a Paragraph for the cover letter with the formatted text
     content = [Paragraph(formatted_text, style)]
     
-    # Build the PDF
     doc.build(content)
     
-    # Move to the beginning of the buffer so it can be sent to the user
     buffer.seek(0)
     return buffer
 
-@app.route('/generate_cover_letter/<job_id>', methods=['GET'])
+@app.route('/api/generate_cover_letter/<job_id>', methods=['GET'])
 def generate_cover_letter_api(job_id):
     try:
-        # Fetch user profile
         user_email = request.args.get("email")
         user = user_profile_collection.find_one({"email": user_email})
         if not user:
@@ -138,22 +130,19 @@ def generate_cover_letter_api(job_id):
         
         name = user.get('firstName') + user.get('lastName')
 
-        # Get resume 
         resume_path = user.get("resume")
-        # Fetch job details 
         job = collection.find_one({"_id": ObjectId(job_id)})
 
         job_role = job["job_title"]
         company_name = job["company"]
         description = job["job_description"]
 
-        # Generate cover letter
         cover_letter = generate_cover_letter(name, resume_path, job_role, company_name, description['basic_req'])
         cover_letter = cover_letter.split("\n", 1)[1]
         pdf_buffer = create_formatted_pdf(cover_letter)
 
         # Send the PDF as a response to the user
-        return send_file(pdf_buffer, mimetype='application/pdf', as_attachment=True, download_name="cover_letter.pdf")
+        return send_file(pdf_buffer, mimetype='application/pdf', as_attachment=True, download_name=request.form["email"].replace("@","_").replace(".","_")+".pdf")
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
